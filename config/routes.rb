f3e18e351e6b2a3d53bed3857a74707d6e3a298f
Tristan Berezowski <bere@Tristans-MacBook-Pro.local>
2019-05-26 15:35:45 +1200

    Session controller prepared for integration

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,10 @@
 Rails.application.routes.draw do
+
+    get '/login' => 'sessions#new'
+    #post '/login' => 'sessions#create'
+    #get '/logout' => 'sessions#destroy'
+
+    namespace :admin do
+
+    end
 end
