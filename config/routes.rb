26f361d8b614cf23d34dff0c8777b160de7f106c
Tristan Berezowski <bere@Tristans-MacBook-Pro.local>
2019-06-08 18:11:21 +1200

    Nav bar clean up, admin and user logging successfully

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
   devise_for :admins
-  get 'home/index'
+    root to: "home#index"
   devise_for :users
     root to: "home#index"
-    get '/login' => 'sessions#new'
+    #get '/login' => 'sessions#new'
     #post '/login' => 'sessions#create'
     #get '/logout' => 'sessions#destroy'
 
