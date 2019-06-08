ece5a42d34cf9959b82a1e6aa9988962e479fa40
Tristan Berezowski <bere@Tristans-MacBook-Pro.local>
2019-06-08 16:01:46 +1200

    controller for home page. Change devise root to home index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
+  get 'home/index'
   devise_for :users
-    root to: "session#new"
+    root to: "home#index"
     get '/login' => 'sessions#new'
     #post '/login' => 'sessions#create'
     #get '/logout' => 'sessions#destroy'
