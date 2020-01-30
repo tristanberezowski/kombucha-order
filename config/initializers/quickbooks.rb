ae116aef3dcad7fd1864d6263a1fc9bd243876b2
Tristan Berezowski <tristanberezowski@gmail.com>
2020-01-30 14:55:13 -0800

    Token refresh request headers added

--- config/initializers/quickbooks.rb
+++ config/initializers/quickbooks.rb
@@ -1,13 +1,4 @@
-# QB_KEY = ENV['QUICKBOOKS_KEY']
-# QB_SECRET = ENV['QUICKBOOKS_SECRET']
-# QB_COMPANY_ID = ENV['QUICKBOOKS_COMPANY_ID']
-
-# ::QB_OAUTH_CONSUMER = OAuth::Consumer.new(OAUTH_CONSUMER_KEY, OAUTH_CONSUMER_SECRET, {
-#   :site                 => "https://oauth.intuit.com",
-#   :request_token_path   => "/oauth/v1/get_request_token",
-#   :authorize_url        => "https://appcenter.intuit.com/Connect/Begin",
-#   :access_token_path    => "/oauth/v1/get_access_token"
-# })
+# Quickbooks.sandbox_mode = true
 
 # oauth_params = {
 #   site: "https://appcenter.intuit.com/connect/oauth2",
@@ -15,5 +6,3 @@
 #   token_url: "https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer"
 # }
 # oauth2_client = OAuth2::Client.new(ENV['OAUTH_CLIENT_ID'], ENV['OAUTH_CLIENT_SECRET'], oauth_params)
-
-# Quickbooks.sandbox_mode = true
